f24d09f588a0049bbacc00493962ce4fef07b71e
akash-k7 <92331024+akash-k7@users.noreply.github.com>
2022-10-27 13:08:34 +0530

    Update script.js

--- imageSlider-Responsive/script.js
+++ imageSlider-Responsive/script.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
         nav:true,
         navText: ["<img src='stock/previous.png'>","<img src='stock/next.png'>"],
         items:1,
-        smartSpeed: 1000
+        smartSpeed: 1000,
+        responsive:{
+            0:{
+                nav:false,
+                smartSpeed:300,
+            },
+            600:{
+                nav:true
+            }
+        }
     });
 });
