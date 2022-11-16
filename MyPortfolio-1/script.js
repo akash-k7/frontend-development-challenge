b86b10837b1dc97b5725f727446cedfdc5dd0b1e
AKASH K <92331024+akash-k7@users.noreply.github.com>
2022-11-16 17:47:47 +0530

    Create script.js

---
+++ MyPortfolio-1/script.js
@@ -0,0 +1,18 @@
+$(".html").animate({
+    width: "95%"
+}, 2500);
+$(".css").animate({
+    width: "90%"
+}, 2500);
+$(".bs").animate({
+    width: "90%"
+}, 2500);
+$(".js").animate({
+    width: "70%"
+}, 2500);
+$(".php").animate({
+    width: "70%"
+}, 2500);
+$(".wp").animate({
+    width: "80%"
+}, 2500);
