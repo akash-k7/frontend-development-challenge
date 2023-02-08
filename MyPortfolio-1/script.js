2d8e30cec07078de4430ddd0d6d25e40b4a2760b
AKASH K <92331024+akash-k7@users.noreply.github.com>
2023-02-08 14:00:03 +0530

    Update script.js

--- MyPortfolio-1/script.js
+++ MyPortfolio-1/script.js
@@ -2,13 +2,13 @@ $(".html").animate({
     width: "95%"
 }, 2500);
 $(".css").animate({
-    width: "90%"
+    width: "92%"
 }, 2500);
 $(".bs").animate({
     width: "90%"
 }, 2500);
 $(".js").animate({
-    width: "70%"
+    width: "71%"
 }, 2500);
 $(".php").animate({
     width: "70%"
