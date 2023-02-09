321fc46f6c6ea46ae6c120bd0b586ec5af106bcc
AKASH K <92331024+akash-k7@users.noreply.github.com>
2023-02-09 16:11:50 +0530

    Update app.js

--- TextUtils/app.js
+++ TextUtils/app.js
@@ -1,11 +1,14 @@
 
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
+      <div className="container my-3">
+        <TextForm heading="Enter the Text to be Analyzed"/>
+      </div>
     </>
   );
 }
