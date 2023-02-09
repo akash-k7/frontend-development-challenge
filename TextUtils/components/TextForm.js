d905424a4cc3d39dd746ec5646831e5fb36de753
AKASH K <92331024+akash-k7@users.noreply.github.com>
2023-02-09 16:54:34 +0530

    Update TextForm.js

--- TextUtils/components/TextForm.js
+++ TextUtils/components/TextForm.js
@@ -6,6 +6,11 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLowClick = ()=>{
+        //console.log("button was clicked");
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
 
     const handleOnChange = (event)=>{
         //console.log("On change");
@@ -15,12 +20,18 @@ export default function TextForm(props) {
     const [text, setText] = useState("Enter the  Text");
 
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"/>
+      <>
+        <div className="container">
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"/>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
-    </div>
+        <div className="container my-3">
+            <h2>summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters.</p>
+        </div>
+      </>
   ) 
 }
