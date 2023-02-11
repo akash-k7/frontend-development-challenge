d1737874e9f64b91e565342f4d0acac321ab7069
AKASH K <92331024+akash-k7@users.noreply.github.com>
2023-02-11 18:40:26 +0530

    Update app.js

--- TextUtils/app.js
+++ TextUtils/app.js
@@ -1,13 +1,29 @@
-
+import React,{useState} from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  
+  const [mode, setMode] = useState('light');
+  
+  const toggleMode = ()=> {
+        if(mode === 'light'){
+          setMode('dark');
+          document.body.style.backgroundColor = '#343a40';
+          document.body.style.color = 'white';
+        }
+        else{
+          setMode('light');
+          document.body.style.backgroundColor = 'white';
+          document.body.style.color = 'black';
+        }
+  }
+  
   return (
     <>
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-        <TextForm heading="Enter the Text to be Analyzed"/>
+        <TextForm heading="Enter the Text to be Analyzed" mode={mode} />
       </div>
     </>
   );
