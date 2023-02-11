dca9081e081b3aac764d63fa987817fb7515d7c6
AKASH K <92331024+akash-k7@users.noreply.github.com>
2023-02-11 18:44:38 +0530

    Update TextForm.js

--- TextUtils/components/TextForm.js
+++ TextUtils/components/TextForm.js
@@ -25,16 +25,16 @@ export default function TextForm(props) {
 
   return (
       <>
-        <div className="container">
+        <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"/>
+                <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'#495057':'white', color: props.mode === 'dark'?'white':'black'}} value={text} onChange={handleOnChange} id="myBox" rows="8"/>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
             <button className="btn btn-primary" onClick={handleLowClick}>Convert To Lowercase</button>
             <button className="btn btn-primary" onClick={handleClearClick}>Clear Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h2>summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters.</p>
         </div>
