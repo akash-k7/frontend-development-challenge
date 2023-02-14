7b616a4ea3ba16e656fe87e41943345e44ff4f36
AKASH K <92331024+akash-k7@users.noreply.github.com>
2023-02-14 15:03:30 +0530

    Update app.js

--- TextUtils/app.js
+++ TextUtils/app.js
@@ -1,29 +1,46 @@
 import React,{useState} from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   
   const [mode, setMode] = useState('light');
+  const [alert,setAlert] = useState(null);
+  
+  const showAlert = (message,type) => {
+    setAlert({
+      message: message,
+      type: type
+      })
+     setTimeout(()=> {
+        setAlert(null)
+        },2000)
+   }
   
   const toggleMode = ()=> {
         if(mode === 'light'){
           setMode('dark');
           document.body.style.backgroundColor = '#343a40';
           document.body.style.color = 'white';
+          showAlert("Dark Mode Enabled", 'success');
+
         }
         else{
           setMode('light');
           document.body.style.backgroundColor = 'white';
           document.body.style.color = 'black';
+          showAlert("Light Mode Enabled", 'success');
+
         }
   }
   
   return (
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the Text to be Analyzed" mode={mode} />
+        <TextForm heading="Enter the Text to be Analyzed" mode={mode} showAlert={showAlert} />
       </div>
     </>
   );
